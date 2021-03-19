457ed3ea05244ef05dac54cafac8519f16773e4c
hyperion-droid <thehyperionprojects@gmail.com>
2021-03-19 20:07:22 +0100

    Added Basic Landing Page - Created webpack config - updated babelrc to support jsx automatic conversion

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import LandingPage from './pages/LandingPage';
+
+render(<LandingPage />, document.getElementById('root'));
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+      <h3>A Simple Landing Page</h3>
+  );
+};
+
+export default LandingPage;
