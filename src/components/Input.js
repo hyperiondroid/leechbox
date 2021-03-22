db884860f4f2e788bb8f30695d76ed1cce0d1a3f
hyperion-droid <thehyperionprojects@gmail.com>
2021-03-22 19:26:22 +0100

    Pass margin and padding to Input component via props - Add DefaultProps to handle missing props

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,9 +7,13 @@ const Input = styled.input`
   color: white;
   font-family: Roobert;
   font-weight: 600;
-  margin: 0.5em 0.25em;
-  padding: 0.25em 0.32em;
+  margin: ${props => props.margin};
+  padding: ${props => props.margin};
   outline: none;
 `
+Input.defaultProps = {
+  margin: '0.5em 0.25em',
+  padding: '0.25em 0.32em'
+}
 
 export default Input
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from '../components/Button'
+import Button from '../components/Button';
 import Input from '../components/Input';
 import Text from '../components/Text';
 import LibraryPlaceholder from '../components/LibraryPlaceholder';
@@ -16,7 +16,7 @@ const LandingPage = () => {
   return (
     <div>
       <Banner/>
-      <Input type="text" spellCheck="false"/>
+      <Input type="text" spellCheck="false" margin="0.5em 1em"/>
       <Button onClick = {startLeach}>Leech!</Button>
       <Text>{log}</Text>
       <LibraryPlaceholder/>
