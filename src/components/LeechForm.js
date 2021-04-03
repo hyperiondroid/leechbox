908e73ad56361fd0a284801584bad7216a378923
hyperion-droid <thehyperionprojects@gmail.com>
2021-04-03 20:07:01 +0200

    Added Leechform placeholder

---
+++ src/components/LeechForm.js
@@ -0,0 +1,15 @@
+
+import styled from 'styled-components';
+
+const LeechFormImpl = ({className, ...misc}) => {
+
+    return(
+        <form>
+            {/* TODO: Move form from Landing page  */}
+        </form>
+    )
+}
+const LeechForm = styled(LeechFormImpl)`
+`
+
+export default LeechForm;
