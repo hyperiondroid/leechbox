29086b74c2a3d408baa3452101048aeff1ab55de
hyperion-droid <thehyperionprojects@gmail.com>
2021-04-04 19:58:09 +0200

    Moving to seperate Form component

--- src/components/LeechForm.js
+++ src/components/LeechForm.js
@@ -1,11 +1,18 @@
 
 import styled from 'styled-components';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import Row from '../components/Row';
 
 const LeechFormImpl = ({className, ...misc}) => {
 
     return(
-        <form>
+        <form autoComplete="off">
             {/* TODO: Move form from Landing page  */}
+            <Row>
+                <Input name="query" type="text" placeholder="Movie/TV" margin="0.5em 1em"/>
+                <Button>Leech</Button>
+            </Row>
         </form>
     )
 }
